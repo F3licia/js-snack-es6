607dc603387fd80e799c05801b82da0ee814fe0c
F3licia <romeofelicia@gmail.com>
2021-04-28 00:12:43 +0200

    aggiunta alternativa filter (vista in aula)

--- Snack 3/myScript.js
+++ Snack 3/myScript.js
@@ -14,14 +14,28 @@ var filteredNo;
 var filteredNo = filterIndex(min, max, numbers);
 console.log(filteredNo)
 
-function filterIndex(a, b, array){
-    result = [];                                    //array risultato
-    numbers.forEach(function(element, index) {      //per ogni elemento prendi index
-        
-        if(index >= a && index <= b){                 //condizione per pushare in "result"
-            result.push(element);                     
-        }
-    });
-    return result                                      //array filtrato
-}
+
+    function filterIndex(a, b, array){
+        result = [];                                    //array risultato
+        numbers.forEach(function(element, index) {      //per ogni elemento prendi index
+            
+            if(index >= a && index <= b){                 //condizione per pushare in "result"
+                result.push(element);                     
+            }
+        });
+        return result                                      //array filtrato
+    }
+
+
+secondFilter(min, max, numbers);
+var check = secondFilter(min, max, numbers);
+console.log(check)
+
+
+
+    function secondFilter(a, b, array){
+    ///inserire la condizione dell'IF nel return per avere i dati verificati
+        return array.filter((element, index) => {return index >= a && index <= b});
+    }
+
 
